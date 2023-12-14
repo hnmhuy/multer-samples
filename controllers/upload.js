536beec768bca84571fdce86117e12e7e4b0813c
Huy Huỳnh Nguyễn Minh <dev.hnmhuy@gmail.com>
2023-12-14 17:58:44 +0700

    Basic upload

--- controllers/upload.js
+++ controllers/upload.js
@@ -1,3 +1,33 @@
 const controller = {};
 
+controller.singleFileUpload = (req, res) => {
+    res.json(
+        {
+            message: "Upload single file success",
+            file: req.file,
+            others: req.body
+        }
+    )
+}
+
+controller.multipleFilesUpload = (req, res) => {
+    res.json(
+        {
+            message: "Upload multiple files success",
+            files: req.files,
+            others: req.body
+        }
+    )
+}
+
+controller.multipleFieldsUpload = (req, res) => {
+    res.json(
+        {
+            message: "Upload multiple files success",
+            files: req.files,
+            others: req.body
+        }
+    )
+}
+
 module.exports = controller;
--- routes/upload.js
+++ routes/upload.js
@@ -1,4 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const controllers = require('../controllers/upload');
+const multer = require('multer');
+const upload = multer({
+    dest: 'public/uploads/'
+})
+
+router.post('/single', upload.single('singleFile'), controllers.singleFileUpload);
+router.post('/multipleFiles', upload.array('multipleFiles', 3), controllers.multipleFilesUpload);
+router.post('/multipleFields', upload.fields([
+    {name: 'multipleFiles1', maxCount: 3},
+    {name: 'multipleFiles2', maxCount: 3}
+]), controllers.multipleFieldsUpload)
 
 module.exports = router;
